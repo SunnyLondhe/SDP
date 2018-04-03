69cfeec47ad17c631cb14c6d424097adcdd36d08
Sunny Londhe <sunnyrlondhe@gmail.com>
2018-04-04 00:20:35 +0200

    Updated Changes with multiple tables

--- JpaHibernateDemo/src/main/java/com/srh_heidelberg/JpaHibernateDemo/App.java
+++ JpaHibernateDemo/src/main/java/com/srh_heidelberg/JpaHibernateDemo/App.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class App {
 	public static void main(String[] args) {
 		
-		int CodeRunValue = 1; // 1 -> for select 2 -> for Insert 3 -> for Update 4 -> for Delete 
+		int CodeRunValue = 4; // 1 -> for select 2 -> for Insert 3 -> for Update 4 -> for Delete 
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("con");
 		EntityManager em = emf.createEntityManager(); // Entity Manager requires Entity Manager Factory
@@ -23,6 +23,7 @@ public class App {
 			stuInsert.setStudent_Name("Moris Abon");
 			stuInsert.setStudent_Address("Mannheim");
 			stuInsert.setStudent_Contact("+49 7777777777");
+			stuInsert.setIdclass(1);
 
 			em.getTransaction().begin(); 	// need to begin the transaction before persist
 			em.persist(stuInsert); 			// persist will save value in app memory
@@ -30,21 +31,22 @@ public class App {
 			System.out.println(stuInsert);
 
 		}else if(CodeRunValue == 3) {
-			student stuUpdate = em.find(student.class, 1);
+			student stuUpdate = em.find(student.class, 3); 
 
 			em.getTransaction().begin(); 			// need to begin the transaction before persist
-			stuUpdate.setStudent_Name("Jhon Mora"); 
+			stuUpdate.setStudent_Name("Jhon Mora");
+			stuUpdate.setIdclass(2);
 			em.getTransaction().commit();			// to save and reflect value into database.
-			System.out.println(stuUpdate);
+			System.out.println("Updated Successfully");
 			
 		}
 		else if(CodeRunValue == 4) {
-			student stuDelete = em.find(student.class, 2);
+			student stuDelete = em.find(student.class, 4);
 
 			em.getTransaction().begin(); 		// need to begin the transaction before persist
-			em.persist(stuDelete); 				// persist will save value in app memory
+			em.remove(stuDelete); 				// persist will save value in app memory
 			em.getTransaction().commit(); 		// to save and reflect value into database.
-			System.out.println(stuDelete);
+			System.out.println("Deleted Successfully");
 			
 		}else {
 			System.out.println("Invalid Operation Selected");
--- JpaHibernateDemo/src/main/java/com/srh_heidelberg/JpaHibernateDemo/student.java
+++ JpaHibernateDemo/src/main/java/com/srh_heidelberg/JpaHibernateDemo/student.java
@@ -1,15 +1,33 @@
 package com.srh_heidelberg.JpaHibernateDemo;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int Student_ID;
 	private String Student_Name;
 	private String Student_Address;
 	private String Student_Contact;
+	private int idclass;
+	public int getIdclass() {
+		return idclass;
+	}
+	public void setIdclass(int idclass) {
+		this.idclass = idclass;
+	}
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "idclass", insertable = false, updatable = false)
+	private classroom classroom;
+	
+	
 	public int getStudent_ID() {
 		return Student_ID;
 	}
@@ -37,7 +55,7 @@ public class student {
 	@Override
 	public String toString() {
 		return "student [Student_ID=" + Student_ID + ", Student_Name=" + Student_Name + ", Student_Address="
-				+ Student_Address + ", Student_Contact=" + Student_Contact + "]";
+				+ Student_Address + ", Student_Contact=" + Student_Contact + ", idClass=" + idclass + "]";
 	}
 	
 }
